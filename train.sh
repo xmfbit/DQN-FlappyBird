336b587a4ef8e8a12ff72147fa92127005750a21
xmfbit <xmfbit@gmail.com>
2017-04-15 17:06:38 +0800

    dev branch

--- train.sh
+++ train.sh
@@ -1,14 +1,14 @@
-lr=0.0001
+lr=0.00005
 gamma=0.99
 batch_size=32
 mem_size=5000
-initial_epsilon=1.
+initial_epsilon=0.2
 final_epsilon=0.1
 observation=100
-exploration=10000
-max_episode=20000
+exploration=1000
+max_episode=2000
 # for fine tuning, uncomment this
-# weight=model_best.pth.tar
+weight=model_best.pth.tar
 
 python main.py --train\
                --lr=$lr\
@@ -20,5 +20,5 @@ python main.py --train\
                --observation=$observation\
                --exploration=$exploration\
                --max_episode=$max_episode\
-               #--weight=$weight\   # for fine tuning, uncomment this
+               --weight=$weight   # for fine tuning, uncomment this
 
